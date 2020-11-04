02f0902c52cbf2ce00cdeab9d3dee8ab912af666
EvgenTen <30858011+EvgenTen@users.noreply.github.com>
2020-11-04 15:07:09 +0200

    Initial commit

---
+++ Assets/scrips/Move_Blue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/* Uparrow=up, downarrow=down, leftarrow=turn left, rightarrow= turn right*/
+public class Move_Blue : MonoBehaviour
+{
+    public GameObject obj;
+    public float range = 5f, moveSpeed = 3f, turnSpeed = 40f;
+
+    public void MoveMe()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            obj.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow))
+            obj.transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            obj.transform.Rotate(Vector3.up * -turnSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            obj.transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+    }
+
+    void Update()
+    {
+
+
+
+        MoveMe();
+
+
+
+
+    }
+}
+
---
+++ Assets/scrips/Move_Red.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/* U=up, D=down, L=turn left, R= turn right*/
+public class Move_Red : MonoBehaviour
+{
+    public GameObject obj;
+    public float range = 5f, moveSpeed = 3f, turnSpeed = 40f;
+
+    public void MoveMe()
+    {
+        if (Input.GetKey(KeyCode.U))
+            obj.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            obj.transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.L))
+            obj.transform.Rotate(Vector3.up * -turnSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.R))
+            obj.transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+    }
+
+    void Update()
+    {
+
+      
+        
+            MoveMe();
+        
+
+    
+
+    }
+}
